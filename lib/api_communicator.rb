cb035029d2f5812661d773a5b03fdb9c9c109c93
“Ranjit <ranjit.saimbi@gmail.com>
2019-03-12 14:44:44 +0000

    star_wars_project

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -7,6 +7,21 @@ def get_character_movies_from_api(character_name)
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
 
+  array_of_films_for_character = []
+
+   response_hash["results"].each do |person|
+    if person["name"] == character_name
+      array_of_films_for_character << person["films"]
+    end
+  end
+
+  array_of_url_data = []
+
+  array_of_films_for_character[0].each do |url|
+    array_of_url_data << JSON.parse(RestClient.get(url))
+  end
+
+  array_of_url_data
   # iterate over the response hash to find the collection of `films` for the given
   #   `character`
   # collect those film API urls, make a web request to each URL to get the info
@@ -18,8 +33,13 @@ def get_character_movies_from_api(character_name)
   #  of movies by title. Have a play around with the puts with other info about a given film.
 end
 
+
 def print_movies(films)
-  # some iteration magic and puts out the movies in a nice list
+  films.each do |film|
+    puts "*" * 20
+    puts film["title"]
+    puts " "
+  end
 end
 
 def show_character_movies(character)
