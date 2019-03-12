9b2c3505044c25bf856bc7c5b3f8c1f81691b8fe
Myrto Konstantinidi <myrtokonst@gmail.com>
2019-03-12 15:02:23 +0000

    Automatically backed up by Learn

--- bin/run.rb
+++ bin/run.rb
@@ -2,7 +2,10 @@
 
 require_relative "../lib/api_communicator.rb"
 require_relative "../lib/command_line_interface.rb"
-
+def run 
 welcome
 character = get_character_from_user
 show_character_movies(character)
+end 
+
+run
